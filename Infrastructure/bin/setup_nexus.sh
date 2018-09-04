8f91b7cb589b68df3320b30182781801a5201bfa
Samm Zhu <14305018+SammZhu@users.noreply.github.com>
2018-09-04 20:21:40 +0800

    Update setup_nexus.sh

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -29,12 +29,13 @@ echo "Setting up Nexus in project $GUID-nexus"
 # oc new-app -f ../templates/nexus.yaml --param .....
 
 # To be Implemented by Student
-
+oc policy add-role-to-user admin system:serviceaccount:gpte-jenkins:jenkins -n ${GUID}-nexus
 #oc create -f ./Infrastructure/templates/nexus.yaml -n ${GUID}-nexus
 oc process -f ./Infrastructure/templates/nexus.yaml \
     -n ${GUID}-nexus \
     -p GUID=${GUID} \
     | oc create -f -
+oc deploy nexus3 --latest
 while : ; do
   echo "Checking if Nexus is Ready..."
   oc get pod -n ${GUID}-nexus|grep '\-2\-'|grep -v deploy|grep "1/1"

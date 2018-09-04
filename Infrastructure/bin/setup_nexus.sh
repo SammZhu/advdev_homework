68539ca2b31477d85a48926dda8bf9b83873b021
Samm Zhu <14305018+SammZhu@users.noreply.github.com>
2018-09-04 21:41:29 +0800

    Update setup_nexus.sh

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -37,6 +37,7 @@ oc process -f ./Infrastructure/templates/nexus.yaml \
     | oc create -n ${GUID}-nexus -f -
 #oc rollout latest dc/nexus3 -n ${GUID}-nexus
 #oc new-app sz-nexus -p GUID=${GUID} -n ${GUID}-nexus
+sleep 20
 while : ; do
   echo "Checking if Nexus is Ready..."
   oc get pod -n ${GUID}-nexus|grep '\-1\-'|grep -v deploy|grep "1/1"

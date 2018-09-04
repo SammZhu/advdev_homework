814671dfdfaeba79debdb69a26db97d364b760ad
Samm Zhu <14305018+SammZhu@users.noreply.github.com>
2018-09-04 10:23:30 +0800

    Update setup_jenkins.sh

--- Infrastructure/bin/setup_jenkins.sh
+++ Infrastructure/bin/setup_jenkins.sh
@@ -29,7 +29,6 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 # To be Implemented by Student
 oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true
 oc new-build --name=jenkins-slave-appdev --dockerfile=$'FROM docker.io/openshift/jenkins-slave-maven-centos7:v3.9\nUSER root\nRUN yum -y install skopeo apb && \nyum clean all\nUSER 1001'
-oc new-app --template=jboss-eap70-openshift:1.7 --param APPLICATION_NAME=mlbparks --param SOURCE_REPOSITORY_URL=https://github.com/SammZhu/advdev_homework.git --param SOURCE_REPOSITORY_REF=master --param CONTEXT_DIR=/MLBParks --param MAVEN_MIRROR_URL=http://nexus3-${GUID}-nexus.apps.${CLUSTER}/repository/maven-all-public
-oc new-app --template=redhat-openjdk18-openshift:1.2 --param APPLICATION_NAME=nationalparks --param SOURCE_REPOSITORY_URL=https://github.com/SammZhu/advdev_homework.git --param SOURCE_REPOSITORY_REF=master --param CONTEXT_DIR=/Nationalparks --param MAVEN_MIRROR_URL=http://nexus3-${GUID}-nexus.apps.${CLUSTER}/repository/maven-all-public
-oc new-app --template=redhat-openjdk18-openshift:1.2 --param APPLICATION_NAME=parksmap --param SOURCE_REPOSITORY_URL=https://github.com/SammZhu/advdev_homework.git --param SOURCE_REPOSITORY_REF=master --param CONTEXT_DIR=/ParksMap --param MAVEN_MIRROR_URL=http://nexus3-${GUID}-nexus.apps.${CLUSTER}/repository/maven-all-public
-oc new-app -f ../templates/pipeline.yaml -p GUID=${GUID},CLUSTER=${CLUSTER}
+oc new-app -f ../templates/mlbparks-pipeline.yaml -p GUID=${GUID},CLUSTER=${CLUSTER}
+oc new-app -f ../templates/nationalparks-pipeline.yaml -p GUID=${GUID},CLUSTER=${CLUSTER}
+oc new-app -f ../templates/parksmap-pipeline.yaml -p GUID=${GUID},CLUSTER=${CLUSTER}

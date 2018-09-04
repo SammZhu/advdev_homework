182205b5877562a8e3ae9fd0bb8f069763b7ed1d
Samm Zhu <14305018+SammZhu@users.noreply.github.com>
2018-09-04 14:28:58 +0800

    Update setup_jenkins.sh

--- Infrastructure/bin/setup_jenkins.sh
+++ Infrastructure/bin/setup_jenkins.sh
@@ -27,8 +27,41 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 # * CLUSTER: the base url of the cluster used (e.g. na39.openshift.opentlc.com)
 
 # To be Implemented by Student
-oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true
+oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true -n ${GUID}-jenkins
+
+# Pause rollout and update config
+oc rollout pause dc jenkins -n ${GUID}-jenkins
+
+# set probe for Jenkins
+oc set probe dc jenkins --readiness --initial-delay-seconds=1200 --timeoutSeconds=480 -n ${GUID}-jenkins
+
+# Resume rollout 
+oc rollout resume dc jenkins -n ${GUID}-jenkins
+
+# Setup Jenkins Maven ImageStream
 oc new-build --name=jenkins-slave-appdev --dockerfile=$'FROM docker.io/openshift/jenkins-slave-maven-centos7:v3.9\nUSER root\nRUN yum -y install skopeo apb && \nyum clean all\nUSER 1001'
-oc new-app -f ../templates/mlbparks-pipeline.yaml
-oc new-app -f ../templates/nationalparks-pipeline.yaml
-oc new-app -f ../templates/parksmap-pipeline.yaml
+
+# Sleep 30 seconds
+sleep 30
+
+# Wait for Jenkins ready
+while : ; do
+  echo "Checking if Jenkins is Ready..."
+  oc get pod -n ${GUID}-jenkins | grep -v "deploy\|build" | grep -q "1/1"
+  [[ "$?" == "1" ]] || break
+  echo "... not quite yet. Sleeping 20 seconds."
+  sleep 20
+done
+
+# Add version 3.9 tag to Jenkins slave ImageStream
+oc tag jenkins-slave-appdev:latest jenkins-slave-appdev:v3.9 -n ${GUID}-jenkins
+
+# Create pipeline build configurations
+oc create -f ./Infrastructure/templates/mlbparks-pipeline.yaml -n ${GUID}-jenkins
+oc create -f ./Infrastructure/templates/nationalparks-pipeline.yaml -n ${GUID}-jenkins
+oc create -f ./Infrastructure/templates/parksmap-pipeline.yaml -n ${GUID}-jenkins
+
+# Setting environmental variables in build configs for pipeline
+oc set env bc/mlbparks-pipeline GUID=${GUID} REPO=${REPO} CLUSTER=${CLUSTER} -n ${GUID}-jenkins
+oc set env bc/nationalparks-pipeline GUID=${GUID} REPO=${REPO} CLUSTER=${CLUSTER} -n ${GUID}-jenkins
+oc set env bc/parksmap-pipeline GUID=${GUID} REPO=${REPO} CLUSTER=${CLUSTER} -n ${GUID}-jenkins

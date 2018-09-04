b4adbed939b7e2954aac182c371871a9ee20832f
Samm Zhu <14305018+SammZhu@users.noreply.github.com>
2018-09-04 11:49:22 +0800

    Update setup_dev.sh

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -12,3 +12,17 @@ echo "Setting up Parks Development Environment in project ${GUID}-parks-dev"
 # Code to set up the parks development project.
 
 # To be Implemented by Student
+# Set up Dev Project
+oc policy add-role-to-user edit system:serviceaccount:${GUID}-jenkins:jenkins -n ${GUID}-parks-dev
+
+oc new-app -e MONGODB_USER=mongodb_user,MONGODB_PASSWORD=mongodb_password,MONGODB_DATABASE=mongodb,MONGODB_ADMIN_PASSWORD=mongodb_admin_password registry.access.redhat.com/rhscl/mongodb-34-rhel7:latest -n ${GUID}-parks-dev
+
+# Set up Dev Application
+oc new-build --binary=true --name="mlbparks" jboss-eap70-openshift:1.7 -n ${GUID}-parks-dev
+oc new-app ${GUID}-parks-dev/mlbparks:latest --name=mlbparks --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev
+oc set triggers dc/mlbparks --remove-all -n ${GUID}-parks-dev
+oc expose dc mlbparks --port 8080 -n ${GUID}-parks-dev
+oc expose svc mlbparks -n ${GUID}-parks-dev
+oc create configmap mlbparks-config --from-literal="application-users.properties=Placeholder" --from-literal="application-roles.properties=Placeholder" -n ${GUID}-parks-dev
+oc set volume dc/mlbparks --add --name=jboss-config --mount-path=/opt/eap/standalone/configuration/application-users.properties --sub-path=application-users.properties --configmap-name=mlbparks-config -n ${GUID}-parks-dev
+oc set volume dc/mlbparks --add --name=jboss-config1 --mount-path=/opt/eap/standalone/configuration/application-roles.properties --sub-path=application-roles.properties --configmap-name=mlbparks-config -n ${GUID}-parks-dev

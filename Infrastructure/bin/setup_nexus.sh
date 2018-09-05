3c60de3a403835a3e87801d6fe7f129cdec20d99
Samm Zhu <14305018+SammZhu@users.noreply.github.com>
2018-09-05 10:07:28 +0800

    Update setup_nexus.sh

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -44,6 +44,7 @@ while : ; do
   echo "...no. Sleeping 10 seconds."
   sleep 10
 done
+sleep 20
 curl -o setup_nexus3.sh -s https://raw.githubusercontent.com/wkulhanek/ocp_advanced_development_resources/master/nexus/setup_nexus3.sh
 chmod +x setup_nexus3.sh
 ./setup_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}')
